95202a9e32feffdefce92bf8fc7f1d3c5dc9a8b3
Dean Baberowski <deanbaberows@gmail.com>
2025-12-30 12:34:48 +1100

    complete lab

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,8 @@
 from sqlalchemy import func
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
@@ -15,7 +17,7 @@ class Game(Base):
     created_at = Column(DateTime(), server_default=func.now())
     updated_at = Column(DateTime(), onupdate=func.now())
 
-    def __repr__(self):
-        return f'Game(id={self.id}, ' + \
-            f'title="{self.title}", ' + \
-            f'platform="{self.platform})"'
+# Configure engine and session, then create tables
+engine = create_engine('sqlite:///games.db')
+Session = sessionmaker(bind=engine)
+Base.metadata.create_all(engine)
